381527c4d047ce5cf68319b35732fff17585fb4b
Darina Beneva <beneva.darina@gmail.com>
2021-04-19 22:40:22 +0300

    More changes

--- 01 - Basics/04 - For Loop/ex01_numbersEndingIn7.js
+++ 01 - Basics/04 - For Loop/ex01_numbersEndingIn7.js
@@ -1,9 +1,21 @@
 function numbersEndingIn7() {
+    let result = '';
+
     for (let i = 0; i < 1000; i++) {
         if (i % 10 == 7) {
-            console.log(i);
+            result += i + '\n';
         }
     }
+
+    return result.trim();
 }
 
-numbersEndingIn7();
+function numbersEndingIn7Arr() {
+    return Array.from(Array(1000), (_, i) => i + 1).filter(x => x % 10 == 7);
+}
+
+console.log(numbersEndingIn7());
+
+console.log('====================');
+
+console.log(numbersEndingIn7());
