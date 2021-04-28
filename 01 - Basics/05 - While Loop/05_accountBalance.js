0ff7d20ed1755e0ef7bae3500dd4479225232087
Darina Beneva <beneva.darina@gmail.com>
2021-04-28 18:14:12 +0300

    More changes

--- 01 - Basics/05 - While Loop/05_accountBalance.js
+++ 01 - Basics/05 - While Loop/05_accountBalance.js
@@ -1,21 +1,22 @@
 function accountBalance(input) {
-    let numberOfTransactions = Number(input[0]);
+    const numberOfTransactions = Number(input[0]);
     let total = 0;
     let i = 1;
+    let output = '';
 
     while (i <= numberOfTransactions) {
         if (Number(input[i]) < 0) {
-            console.log('Invalid operation!');
+            output += `Invalid operation!\n`;
             break;
         } else {
             total += Number(input[i]);
-            console.log(`Increase: ${Number(input[i]).toFixed(2)}`);
+            output += `Increase: ${Number(input[i]).toFixed(2)}\n`;
         }
 
         i++;
     }
 
-    console.log(`Total: ${total.toFixed(2)}`);
+    return output + `Total: ${total.toFixed(2)}`;
 }
 
-accountBalance([5, 120, 45.55, -150]);
+console.log(accountBalance([5, 120, 45.55, -150]));
