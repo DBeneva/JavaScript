89cd55d7fab9082f375eea52d90d2b0d9db62dc2
Darina Beneva <beneva.darina@gmail.com>
2021-05-05 19:01:52 +0300

    More changes

--- 03 - Advanced/03 - Objects and Composition/02_townPopulation.js
+++ 03 - Advanced/03 - Objects and Composition/02_townPopulation.js
@@ -1,23 +1,41 @@
 function townPopulation(input) {
     const towns = {};
+    const output = [];
 
     input.forEach(line => {
-        let [name, population] = line.split(' <-> ');
-        
-        if (!towns[name]) {
-            towns[name] = 0;
-        }
-
+        const [name, population] = line.split(' <-> ');
+        towns[name] = towns[name] ? towns[name] : 0;
         towns[name] += Number(population);
     });
 
     for (let [name, population] of Object.entries(towns)) {
-        console.log(`${name} : ${population}`);
+        output.push(`${name} : ${population}`);
     }
+
+    return output.join('\n');
 }
 
-townPopulation(['Istanbul <-> 100000',
-'Honk Kong <-> 2100004',
-'Jerusalem <-> 2352344',
-'Mexico City <-> 23401925',
-'Istanbul <-> 1000']);
+function townPopulationReduce(input) {
+    return Object.entries(input
+        .reduce((acc, curr) => Object.assign({
+            ...acc,
+            [curr.split(' <-> ')[0]]: (acc[curr.split(' <-> ')[0]] || 0) + Number(curr.split(' <-> ')[1])
+        }
+        ), {}))
+        .map(([k, v]) => `${k}: ${v}`)
+        .join('\n');
+}
+
+console.log(townPopulation(['Istanbul <-> 100000',
+    'Honk Kong <-> 2100004',
+    'Jerusalem <-> 2352344',
+    'Mexico City <-> 23401925',
+    'Istanbul <-> 1000']));
+
+console.log('====================');
+
+console.log(townPopulationReduce(['Istanbul <-> 100000',
+    'Honk Kong <-> 2100004',
+    'Jerusalem <-> 2352344',
+    'Mexico City <-> 23401925',
+    'Istanbul <-> 1000']));
