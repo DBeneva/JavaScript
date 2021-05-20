494704ed7f01db2aaf15a0ecc317758025552d2e
Darina Beneva <beneva.darina@gmail.com>
2021-05-20 10:00:21 +0300

    More changes

--- 05 - JS Back-End/01 - Intro to NodeJS/01 - Lab/Project/handlers/home.js
+++ 05 - JS Back-End/01 - Intro to NodeJS/01 - Lab/Project/handlers/home.js
@@ -1,28 +1,29 @@
-const url = require('url');
 const fs = require('fs');
 const path = require('path');
 const cats = require('../data/cats.json');
-const { dirname } = require('node:path');
+const breeds = require('../data/breeds.json');
 
 module.exports = (req, res) => {
-    const pathname = url.parse(req.url).pathname;
+    const url = new URL(req.url, 'http://localhost');
+    const pathname = url.pathname;
 
     if (pathname == '/' && req.method == 'GET') {
-        let filePath = path.normalize(path.join(__dirname, '../views/home/index.html'));
+        const filePath = path.normalize(path.join(__dirname, '../views/home/index.html'));
 
         fs.readFile(filePath, (err, data) => {
             if (err) {
                 console.log(err);
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
                 res.write('File not found');
                 res.end();
 
                 return;
             }
 
-            res.writeHead();
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
         });
-
     } else {
         return true;
     }
--- 05 - JS Back-End/01 - Intro to NodeJS/01 - Lab/Project/index.js
+++ 05 - JS Back-End/01 - Intro to NodeJS/01 - Lab/Project/index.js
@@ -1,8 +1,31 @@
 const http = require('http');
 const port = 3000;
+const handlers = require('./handlers/index');
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello, JS world!');
-    res.end();
+    for (let handler of handlers) {
+        if (!handler(req, res)) {
+            break;
+        }
+        handler(req, res);
+    }
 }).listen(port);
+
+// (req, res) => {
+//     if (pathname == '/' && req.method == 'GET') {
+//         fs.readFile(filePath, (err, data) => {
+//             if (err) {
+//                 res.writeHead(404, { 'Content-Type': 'text/plain' });
+//                 res.write('File not found');
+//                 res.end();
+//                 return;
+//             }
+
+//             res.writeHead(200, { 'Content-Type': 'text/html' });
+//             res.write(data);
+//             res.end();
+//         });
+//     } else {
+//         return true;
+//     }
+// }
