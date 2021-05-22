be67b968cc2bf5f3b964a2d50b7602462b28df00
Darina Beneva <beneva.darina@gmail.com>
2021-05-22 09:56:07 +0300

    More changes

--- 05 - JS Back-End/01 - Intro to NodeJS/01 - Lab/Demo/controllers/createController.js
+++ 05 - JS Back-End/01 - Intro to NodeJS/01 - Lab/Demo/controllers/createController.js
@@ -1,15 +1,27 @@
 const formidable = require('formidable');
 const database = require('../util/database');
+const qs = require('querystring');
 
 module.exports = (req, res) => {
-    const form = new formidable.IncomingForm();
+    // const form = new formidable.IncomingForm();
 
-    form.parse(req, (err, fields, files) => {
-        database.addItem(fields);
+    // form.parse(req, (err, fields, files) => {
+    //     database.addItem(fields);
 
-        res.writeHead(301, {
-            'Location': '/catalog'
-        });
-        res.end();
+    //     res.writeHead(301, {
+    //         'Location': '/catalog'
+    //     });
+    //     res.end();
+    // });
+
+    let body = '';
+    req.on('data', data => {
+        body += data.toString(); // 'name=item&serial=1234'
     });
+    req.on('end', () => console.log(body));
+
+    res.writeHead(301, {
+        'Location': '/catalog'
+    });
+    res.end();
 };
