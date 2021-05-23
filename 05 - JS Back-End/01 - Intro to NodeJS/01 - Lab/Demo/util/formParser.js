dda33e59a099302b7288bc26eec0e0664e7a442c
Darina Beneva <beneva.darina@gmail.com>
2021-05-23 15:07:24 +0300

    More changes

---
+++ 05 - JS Back-End/01 - Intro to NodeJS/01 - Lab/Demo/util/formParser.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+async function parse(req) {
+    return new Promise((resolve, reject) => {
+        let body = '';
+
+        req.on('data', data => body += data);
+        req.on('end', () => {
+            let result;
+
+            try {
+                result = body
+                .split('&')
+                .map(p => p.split('='))
+                .reduce((acc, curr) => Object.assign(acc, { [curr[0]]: curr[1] }), {});
+            
+                resolve(result);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    });
+}
+
+async function demo() {
+    const src = fs.createReadStream('./data.txt');
+    const result = await parse(src);
+
+    console.log(result);
+}
+
+demo();
