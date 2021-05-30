d486b89cc0d31c1348d3a26a37d3221644ae6ed1
Darina Beneva <beneva.darina@gmail.com>
2021-05-30 13:51:51 +0300

    More changes

--- 01 - Basics/05 - While Loop/09_graduation2.js
+++ 01 - Basics/05 - While Loop/09_graduation2.js
@@ -1,5 +1,5 @@
 function graduation2(input) {
-    let name = input[0];
+    const name = input[0];
     let gradeTotal = 0;
     let years = 1;
     let expelled = 0;
@@ -34,38 +34,9 @@ function graduation2(input) {
 }
 
 function graduation2Arr(input) {
-    let name = input[0];
-    let gradeTotal = 0;
-    let years = 1;
-    let expelled = 0;
-    let yearExpelled = 0;
-    let i = 1;
+    const name = input.shift();
 
-    while (years <= 12) {
-        let grade = Number(input[i]);
-        i++;
-
-        if (grade < 4) {
-            expelled++;
-
-            if (expelled >= 2) {
-                yearExpelled += years;
-                break;
-            } else {
-                continue;
-            }
-        }
-
-        gradeTotal += grade;
-        years++;
-    }
-
-    if (expelled >= 2) {
-        return `${name} has been excluded at ${yearExpelled} grade`;
-    } else {
-        let averageGrade = gradeTotal / 12;
-        return `${name} graduated. Average grade: ${averageGrade.toFixed(2)}`;
-    }
+    
 }
 
 
