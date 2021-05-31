070516dacd9f0c77b07977fe1f477cdd6313beac
Darina Beneva <beneva.darina@gmail.com>
2021-05-31 10:36:13 +0300

    More changes

--- 01 - Basics/05 - While Loop/10_moving.js
+++ 01 - Basics/05 - While Loop/10_moving.js
@@ -16,10 +16,38 @@ function moving(input) {
     }
 
     if (freeSpace > boxes) {
-        console.log(`${freeSpace - boxes} Cubic meters left.`);
+        return `${freeSpace - boxes} Cubic meters left.`;
     } else {
-        console.log(`No more free space! You need ${boxes - freeSpace} Cubic meters more.`);
+        return `No more free space! You need ${boxes - freeSpace} Cubic meters more.`;
     }
 }
 
-moving([10, 1, 2, 4, 6, 'Done']);
+function movingArr(input) {
+    let width = Number(input[0]);
+    let length = Number(input[1]);
+    let height = Number(input[2]);
+    let boxes = 0;
+    let freeSpace = width * length * height;
+    let i = 3;
+
+    while (input[i] != 'Done') {
+        boxes += Number(input[i]);
+        i++;
+
+        if (freeSpace <= boxes) {
+            break;
+        }
+    }
+
+    if (freeSpace > boxes) {
+        return `${freeSpace - boxes} Cubic meters left.`;
+    } else {
+        return `No more free space! You need ${boxes - freeSpace} Cubic meters more.`;
+    }
+}
+
+console.log(moving([10, 1, 2, 4, 6, 'Done']));
+
+console.log('====================');
+
+console.log(movingArr([10, 1, 2, 4, 6, 'Done']));
