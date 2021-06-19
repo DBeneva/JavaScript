224de84c05c53141349c144d663b40adf6038cdb
Darina Beneva <beneva.darina@gmail.com>
2021-06-19 23:25:03 +0300

    More changes

--- 05 - JS Back-End/07 - Exam Preparation/01 - Exam Preparation 17.06.21/services/hotel.js
+++ 05 - JS Back-End/07 - Exam Preparation/01 - Exam Preparation 17.06.21/services/hotel.js
@@ -1,13 +1,17 @@
 const Hotel = require('../models/Hotel');
 
-async function createHotel(hotelData) {
-    const hotel = new Hotel(hotelData)
-    await hotel.save();
-    return hotel;
-}
-
 async function getAllHotels() {
     const hotel = await Hotel.find({});
 
+    return hotel;
+}
+
+async function getHotelById(id) {
+
+}
+
+async function createHotel(hotelData) {
+    const hotel = new Hotel(hotelData)
+    await hotel.save();
     return hotel;
 }
