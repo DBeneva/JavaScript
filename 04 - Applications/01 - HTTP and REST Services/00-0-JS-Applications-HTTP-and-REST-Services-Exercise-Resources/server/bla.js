bcaee39b568bdf7fda90153494a74674d65ec737
Darina Beneva <beneva.darina@gmail.com>
2021-07-05 17:39:54 +0300

    Updated JS document

--- 04 - Applications/01 - HTTP and REST Services/00-0-JS-Applications-HTTP-and-REST-Services-Exercise-Resources/server/bla.js
+++ 04 - Applications/01 - HTTP and REST Services/00-0-JS-Applications-HTTP-and-REST-Services-Exercise-Resources/server/bla.js
@@ -1,3 +1,4 @@
-sayHi();
-
-function sayHi() { console.log('hi'); }
+function pow(pow, num) { return num ** pow; }
+const sqr = pow.bind(null, 2);
+console.log([2, 4, 12, 37].find(e => e > 100)); 
+ 
