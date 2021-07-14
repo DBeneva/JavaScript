d0ff016b1345cece98db52130ac8324315d74dae
Darina Beneva <beneva.darina@gmail.com>
2021-07-14 15:11:37 +0300

    Updated JS document

--- 06 - Angular/02 - Components/02 - Lab/lecture2/src/app/app.component.ts
+++ 06 - Angular/02 - Components/02 - Lab/lecture2/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'lecture2';
+  title = '3';
+  classes = ['test', 'test-1'];
+  showText = true;
+
+  changeTitleHandler(inputEl: HTMLInputElement): void {
+    this.title = inputEl.value;
+    inputEl.value = '';
+  }
+
+  toggleText(event: MouseEvent, ...args: number[]): void {
+    event.preventDefault();
+    this.showText = !this.showText;
+    console.log(args);
+    
+  }
+  // title = 3;
+  // users = [
+  //   {
+  //     name: 'Ivan 1',
+  //     age: 21
+  //   },
+  //   {
+  //     name: 'Ivan 2',
+  //     age: 22
+  //   },
+  //   {
+  //     name: 'Ivan 3',
+  //     age: 23
+  //   }
+  // ];
+  // btnClickHandler(): void {
+  //   console.log('Button was clicked');    
+  //   //this.title++;
+  //   const current = this.title++;
+  //   this.users.push({ name: `Ivan ${current}`, age: 20 + current });
+  // } 
 }
