a943995cafaaabaf51e4645476068dde955172fc
Darina Beneva <beneva.darina@gmail.com>
2021-07-16 13:25:53 +0300

    Updated JS document

--- 06 - Angular/01 - Intro to Angular and Typescript/02 - Exercises/Tickets.ts
+++ 06 - Angular/01 - Intro to Angular and Typescript/02 - Exercises/Tickets.ts
@@ -1,18 +1,12 @@
 class Ticket {
-    destination: string;
-    price: number;
-    status: string;
-    constructor(destination: string, price: string, status: string) {
-        this.destination = destination;
-        this.price = Number(price);
-        this.status = status;
+    constructor(public destination: string, public price: number, public status: string) {
     }
 }
 
 function sortTickets(ticketDescriptions: string[], criteria: string) {
     const tickets = ticketDescriptions.map(t => {
         const [destination, price, status] = t.split('|');
-        return new Ticket(destination, price, status);
+        return new Ticket(destination, Number(price), status);
     });
 
     return tickets.sort((a, b) => a[criteria] - b[criteria]);
