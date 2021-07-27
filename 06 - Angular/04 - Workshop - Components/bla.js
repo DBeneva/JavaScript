0a3651a91305756d78f6ee524d7b3219e0bd95c7
Darina Beneva <beneva.darina@gmail.com>
2021-07-27 17:36:33 +0300

    Updated JS document

--- 06 - Angular/04 - Workshop - Components/bla.js
+++ 06 - Angular/04 - Workshop - Components/bla.js
@@ -1,7 +1,15 @@
 let a = 1;
 let b = a++ + ++a;
-console.log(a, b);
+console.log(a, b); // 3 4
 
 let c = 1;
 let d = c++ - ++c;
-console.log(c, d);
+console.log(c, d); // 3 -2
+
+let e = 3;
+let f = e++;
+console.log(e, f); // 4 3
+
+let g = 3;
+let h = ++g;
+console.log(g, h); // 4 4
