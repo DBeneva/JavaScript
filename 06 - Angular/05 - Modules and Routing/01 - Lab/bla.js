460fc22a3f309bac55f6b9430095321164787c3d
Darina Beneva <beneva.darina@gmail.com>
2021-08-05 13:30:49 +0300

    Updated JS document

---
+++ 06 - Angular/05 - Modules and Routing/01 - Lab/bla.js
@@ -0,0 +1,4 @@
+console.log((47.445).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}));
+console.log((1234.56).toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }));
+console.log((1234.56).toFixed(2));
+
