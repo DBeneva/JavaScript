969bd90d37476f7eca152bcfbacef5a5026be132
Darina Beneva <beneva.darina@gmail.com>
2021-08-07 16:15:13 +0300

    Updated JS document

--- 06 - Angular/05 - Modules and Routing/01 - Lab/bla.js
+++ 06 - Angular/05 - Modules and Routing/01 - Lab/bla.js
@@ -1,2 +1,3 @@
-console.log('\u00F1'.normalize() == '\u006E\u0303'.normalize());
-console.log('\u006E\u0303');
+let fruits = ['a', 'b', 'c'];
+
+console.log(['a', new Date()].toLocaleString());
---
+++ 06 - Angular/05 - Modules and Routing/01 - Lab/bla.ts
@@ -0,0 +1,3 @@
+let fruits = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+console.log(fruits.copyWithin(0));
