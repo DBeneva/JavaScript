9e532fd532d209de88faf8de558a2d992da1a034
Darina Beneva <beneva.darina@gmail.com>
2021-08-08 20:14:43 +0300

    Updated JS document

--- 06 - Angular/05 - Modules and Routing/01 - Lab/bla.ts
+++ 06 - Angular/05 - Modules and Routing/01 - Lab/bla.ts
@@ -3,9 +3,11 @@ enum PrintMedia {
     Newsletter = 11,
     Magazine = Newspaper * 5 + 800,
     Book = getMedia('Not Forbes'),
-    Billboard
+    Billboard = 0
 }
 
 function getMedia(mediaName: (string | number | boolean)): PrintMedia {
     if (mediaName == 'Forbes') return PrintMedia.Magazine;
 }
+
+const a = ['text', 3, '23'];
