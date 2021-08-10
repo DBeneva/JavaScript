76e06fcc6e8b9cb4393c9247c3589b9ca3885767
Darina Beneva <beneva.darina@gmail.com>
2021-08-10 12:43:25 +0300

    Updated JS document

--- 06 - Angular/05 - Modules and Routing/01 - Lab/bla.ts
+++ 06 - Angular/05 - Modules and Routing/01 - Lab/bla.ts
@@ -1,6 +1,12 @@
-function add(a: number, b: number): number;
-function add(a: string): string;
-
-function add(a: any, b: any): any {
-    return a + b;
+class Car {
+    constructor(public name: string) { }
+    run(speed: number = 0) { return `${this.name} is fast: ${speed}` }
 }
+
+class Honda extends Car {
+    constructor(public name: string) { super(name); }
+    run(speed = 150) { return `A Honda started. ${super.run(speed)}` }
+}
+
+const honda = new Honda('Honda City');
+honda.run();
