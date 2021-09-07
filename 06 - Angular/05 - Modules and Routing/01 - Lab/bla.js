85434e94931293684fd981fb6cac879795fb2137
Darina Beneva <beneva.darina@gmail.com>
2021-09-07 18:59:08 +0300

    Updated JS document

--- 06 - Angular/05 - Modules and Routing/01 - Lab/bla.js
+++ 06 - Angular/05 - Modules and Routing/01 - Lab/bla.js
@@ -1,9 +1,19 @@
-"use strict";
-class KeyValue {
-    constructor(key, val) {
-        this.key = key;
-        this.val = val;
-    }
+function winningTicket(input) {
+    console.log(input
+        .split(/ *, +/g)
+        .map(t => {
+            const pattern = t.slice(0, 10).match(new RegExp(/\${6,}|@{6,}|#{6,}\^{6,}/)) ?
+                t.slice(0, 10).match(new RegExp(/\${6,}|@{6,}|#{6,}\^{6,}/))[0] : null;
+
+            return t.length == 20 ?
+                t.slice(10).match(pattern) ?
+                    `ticket "${t}" - ${pattern.length}${pattern[0]}${pattern.length == 10 ? ' Jackpot!' : ''}` :
+                    `ticket "${t}" - no match` :
+                'invalid ticket';
+        })
+        .join('\n'));
 }
-const kvp = new KeyValue(1, 'Ben');
-console.log(kvp.val); // 'Ben'
+
+winningTicket('Cash$@$$$$Ca$$$$$$sh');
+winningTicket('$$$$$$$$$$$$$$$$$$$$   ,   aabb  ,     th@@@@@@eemo@@@@@@ey');
+winningTicket('validticketnomatch:(');
