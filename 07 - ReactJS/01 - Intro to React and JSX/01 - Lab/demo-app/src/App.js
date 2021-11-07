3b883b99a41c43702e12434c8735440f6912589f
Darina Beneva <beneva.darina@gmail.com>
2021-11-07 15:02:33 +0200

    React demo

--- 07 - ReactJS/01 - Intro to React and JSX/01 - Lab/demo-app/src/App.js
+++ 07 - ReactJS/01 - Intro to React and JSX/01 - Lab/demo-app/src/App.js
@@ -1,61 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
       <div className="back-to-top"></div>
 
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
-          <div className="container">
-            <a href="#" className="navbar-brand">Seo<span className="text-primary">Gram.</span></a>
-
-            <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="navbar-collapse collapse" id="navbarContent">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="index.html">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="about.html">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="service.html">Services</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="blog.html">Blog</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="contact.html">Contact</a>
-                </li>
-                <li className="nav-item">
-                  <a className="btn btn-primary ml-lg-2" href="#">Free Analytics</a>
-                </li>
-              </ul>
-            </div>
-
-          </div>
-        </nav>
-
-        <div className="container">
-          <div className="page-banner home-banner">
-            <div className="row align-items-center flex-wrap-reverse h-100">
-              <div className="col-md-6 py-5 wow fadeInLeft">
-                <h1 className="mb-4">Let's Check and Optimize your website!</h1>
-                <p className="text-lg text-grey mb-5">Ignite the most powerfull growth engine you have ever built for your company</p>
-                <a href="#" className="btn btn-primary btn-split">Watch Video <div className="fab"><span className="mai-play"></span></div></a>
-              </div>
-              <div className="col-md-6 py-5 wow zoomIn">
-                <div className="img-fluid text-center">
-                  <img src="/img/banner_image_1.svg" alt="" />
-                </div>
-              </div>
-            </div>
-            <a href="#about" className="btn-scroll" data-role="smoothscroll"><span className="mai-arrow-down"></span></a>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <div className="page-section">
         <div className="container">
@@ -390,51 +341,7 @@ function App() {
           </div>
         </div>
       </div>
-
-      <footer className="page-footer bg-image" style={{backgroundImage: 'url(/img/world_pattern.svg)'}}>
-        <div className="container">
-          <div className="row mb-5">
-            <div className="col-lg-3 py-3">
-              <h3>SEOGram</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero amet, repellendus eius blanditiis in iusto eligendi iure.</p>
-
-              <div className="social-media-button">
-                <a href="#"><span className="mai-logo-facebook-f"></span></a>
-                <a href="#"><span className="mai-logo-twitter"></span></a>
-                <a href="#"><span className="mai-logo-google-plus-g"></span></a>
-                <a href="#"><span className="mai-logo-instagram"></span></a>
-                <a href="#"><span className="mai-logo-youtube"></span></a>
-              </div>
-            </div>
-            <div className="col-lg-3 py-3">
-              <h5>Company</h5>
-              <ul className="footer-menu">
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Career</a></li>
-                <li><a href="#">Advertise</a></li>
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Help & Support</a></li>
-              </ul>
-            </div>
-            <div className="col-lg-3 py-3">
-              <h5>Contact Us</h5>
-              <p>203 Fake St. Mountain View, San Francisco, California, USA</p>
-              <a href="#" className="footer-link">+00 1122 3344 5566</a>
-              <a href="#" className="footer-link">seogram@temporary.com</a>
-            </div>
-            <div className="col-lg-3 py-3">
-              <h5>Newsletter</h5>
-              <p>Get updates, news or events on your mail.</p>
-              <form action="#">
-                <input type="text" className="form-control" placeholder="Enter your email.." />
-                <button type ="submit" class ="btn btn-success btn-block mt-2">Subscribe</button>
-              </form>
-            </div>
-          </div>
-
-          <p className="text-center" id="copyright">Copyright &copy; 2020. This template design and develop by <a href="https://macodeid.com/" target="_blank">MACode ID</a></p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
