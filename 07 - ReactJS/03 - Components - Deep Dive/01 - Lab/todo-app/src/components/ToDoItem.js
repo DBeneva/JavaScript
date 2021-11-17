50fa174bf639295f78067eefa9d298ddfceeebcb
Darina Beneva <beneva.darina@gmail.com>
2021-11-17 10:34:14 +0200

    React components demo

--- 07 - ReactJS/03 - Components - Deep Dive/01 - Lab/todo-app/src/components/ToDoItem.js
+++ 07 - ReactJS/03 - Components - Deep Dive/01 - Lab/todo-app/src/components/ToDoItem.js
@@ -1,5 +1,25 @@
-function ToDoItem({text}) {
-    return <li>{text}</li>;
+import styles from './ToDoItem.module.css';
+import { useEffect } from 'react';
+
+function ToDoItem({
+    todo,
+    onClick,
+    onDelete
+}) {
+    useEffect(() => {
+        console.log(`${todo.id} mounted`);
+
+        return () => {
+            console.log(`${todo.id} unmounted`);
+        };
+    }, []);
+
+    return (
+        <li onClick={() => onClick(todo.id)} className={styles["todo-item"]}>
+            {todo.text} 
+            <button onClick={() => onDelete(todo.id)}>x</button>
+        </li>
+    );
 }
 
 export default ToDoItem;
--- 07 - ReactJS/03 - Components - Deep Dive/01 - Lab/todo-app/src/components/ToDoList.js
+++ 07 - ReactJS/03 - Components - Deep Dive/01 - Lab/todo-app/src/components/ToDoList.js
@@ -1,16 +1,55 @@
+import { useState } from 'react';
+import uniqid from 'uniqid';
 import ToDoItem from "./ToDoItem";
 
 function ToDoList() {
-    const todos = [
-        { id: 1, text: 'Clean my room' },
-        { id: 2, text: 'Wash the dishes' },
-        { id: 3, text: 'Go to the gym!' }
-    ];
+    const [todos, setTodos] = useState([
+        { id: 'kw2krau0', text: 'Clean my room', isDone: false },
+        { id: 'kw2krn18', text: 'Wash the dishes', isDone: false },
+        { id: 'kw2kryv1', text: 'Go to the gym!', isDone: false }
+    ]);
+
+    const onInputBlur = (e) => {
+        if (e.target.value === '') return;
+        
+        let todo = {
+            id: uniqid(),
+            text: e.target.value,
+            isDone: false
+        };
+
+        setTodos(state => [...state, todo]);
+        e.target.value = '';
+    };
+
+    const deleteTodoItemClickHandler = (id) => {
+        setTodos(state => state.filter(todo => todo.id !== id));
+    };
+
+    const toggleTodoItemClickHandler = (id) => {
+        setTodos(state => {
+            let selectedToDo = state.find(x => x.id === id);
+            let toggledToDo = {...selectedToDo, isDone: !selectedToDo.isDone};
+            let restToDos = state.filter(x => x.id !== id);
+
+            return [...restToDos, toggledToDo];
+        });
+    };
 
     return (
-        <ul>
-            {todos.map(x => <ToDoItem key={x.id} text={x.text} />)}
-        </ul>
+        <>
+            <label htmlFor="todo">Add todo: </label>
+            <input type="text" onBlur={onInputBlur} name="todo" />
+            <ul>
+                {todos.map(x =>
+                    <ToDoItem
+                        key={x.id}
+                        todo={x}
+                        onClick={toggleTodoItemClickHandler}
+                        onDelete={deleteTodoItemClickHandler} />
+                )}
+            </ul>
+        </>
     );
 }
 
