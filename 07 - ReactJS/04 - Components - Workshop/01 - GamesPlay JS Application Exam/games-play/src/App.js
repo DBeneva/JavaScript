e0e05d98f2bf2f6ca62d14b30a9093573721ad96
Darina Beneva <beneva.darina@gmail.com>
2021-11-19 14:20:12 +0200

    Add simple page routing

--- 07 - ReactJS/04 - Components - Workshop/01 - GamesPlay JS Application Exam/games-play/src/App.js
+++ 07 - ReactJS/04 - Components - Workshop/01 - GamesPlay JS Application Exam/games-play/src/App.js
@@ -1,22 +1,36 @@
+import { useState } from 'react';
+
 import Header from "./components/Header";
 import WelcomeWorld from "./components/WelcomeWorld";
 import GameCatalog from "./components/GameCatalog";
 import CreateGame from "./components/CreateGame";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
+  const [page, setPage] = useState('/home');
   const routes = {
-    '/home': WelcomeWorld,
-    '/games': GameCatalog,
-    '/create-game': CreateGame
+    '/home': <WelcomeWorld />,
+    '/games': <GameCatalog />,
+    '/create-game': <CreateGame />,
+    '/login': <Login />,
+    '/register': <Register />
+  };
+
+  const navigationChangeHandler = (path) => {
+    console.log(path);
+    setPage(path);
   };
 
   return (
     <div id="box">
 
-      <Header />
+      <Header
+        navigationChangeHandler={navigationChangeHandler}
+      />
 
       <main id="main-content">
-        <WelcomeWorld />
+        { routes[page] || <h2>No Page Found!</h2> }
       </main>
 
     </div>
--- 07 - ReactJS/04 - Components - Workshop/01 - GamesPlay JS Application Exam/games-play/src/components/Header.js
+++ 07 - ReactJS/04 - Components - Workshop/01 - GamesPlay JS Application Exam/games-play/src/components/Header.js
@@ -1,16 +1,18 @@
-function Header() {
+function Header({
+    navigationChangeHandler
+}) {
     const onHeaderClick = (e) => {
         e.preventDefault();
         
         if (e.target.tagName == 'A') {
             let url = new URL(e.target.href);
-            console.log(url.pathname);
+            navigationChangeHandler(url.pathname);
         }
     };
 
     return (
         <header onClick={onHeaderClick}>
-            <h1><a className="home" href="/">GamesPlay</a></h1>
+            <h1><a className="home" href="/home">GamesPlay</a></h1>
             <nav>
                 <a href="/games">All games</a>
 
