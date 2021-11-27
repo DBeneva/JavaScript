9317ed1fecf3bacde148c05291d1a89947f3f97c
Darina Beneva <beneva.darina@gmail.com>
2021-11-27 12:18:32 +0200

    Created form

--- 07 - ReactJS/07 - Forms/01 - Lab/src/App.js
+++ 07 - ReactJS/07 - Forms/01 - Lab/src/App.js
@@ -1,7 +1,23 @@
+import './App.css';
+
 function App() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      
+      <form method="POST" onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input type="text" name="username" id="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <input type="submit" name="" value="Login" />
+      </form>
     </div>
   );
 }
