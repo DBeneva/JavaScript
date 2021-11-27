5747a9c2bddcee3fdded1db2383047063a8a5d6b
Darina Beneva <beneva.darina@gmail.com>
2021-11-27 13:58:56 +0200

    Dynamically set select options

--- 07 - ReactJS/07 - Forms/01 - Lab/src/App.js
+++ 07 - ReactJS/07 - Forms/01 - Lab/src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [services, setServices] = useState([]);
   const [isValid, setIsValid] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:3030/jsonstore/services')
+      .then(res => res.json())
+      .then(res => {
+        setServices(Object.values(res));
+      });
+  }, []);
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
-    const { username, password, rememberMe } = Object.fromEntries(formData);
+    const { username, password, rememberMe, services } = Object.fromEntries(formData);
 
     console.log(username);
     console.log(password);
     console.log(Boolean(rememberMe));
+    console.log(services);
   };
 
   const onChange = (e) => {
@@ -42,8 +52,16 @@ function App() {
           <label htmlFor="password">Password: </label>
           <input type="password" name="password" id="password" />
         </div>
-        <label htmlFor="rememberMe">Remember me </label>
-        <input type="checkbox" name="rememberMe" id="rememberMe" defaultChecked />
+        <div>
+          <label htmlFor="services">Services </label>
+          <select name="services" id="services">
+            {services.map(x => <option key={x._id} value={x._id}>{x.name}</option>)}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="rememberMe">Remember me </label>
+          <input type="checkbox" name="rememberMe" id="rememberMe" defaultChecked />
+        </div>
         <input type="submit" value="Login" />
       </form>
     </div>
