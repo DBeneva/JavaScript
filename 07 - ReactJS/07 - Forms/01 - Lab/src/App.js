dc69563e5a2b5a445d928e3dd42973d68fba14c1
Darina Beneva <beneva.darina@gmail.com>
2021-11-27 13:29:34 +0200

    Uncontrolled components

--- 07 - ReactJS/07 - Forms/01 - Lab/src/App.js
+++ 07 - ReactJS/07 - Forms/01 - Lab/src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isValid, setIsValid] = useState(true);
+
   const submitHandler = (e) => {
     e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const { username, password, rememberMe } = Object.fromEntries(formData);
+
+    console.log(username);
+    console.log(password);
+    console.log(Boolean(rememberMe));
+  };
+
+  const onChange = (e) => {
+    const currentUsername = e.target.value;
+    if (currentUsername.length < 4) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
   };
 
   return (
@@ -10,13 +29,22 @@ function App() {
       <form method="POST" onSubmit={submitHandler}>
         <div>
           <label htmlFor="username">Username: </label>
-          <input type="text" name="username" id="username" />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            defaultValue="Pesho"
+            onChange={onChange}
+          />
         </div>
+        {!isValid && <div style={{ color: 'red' }}>Username is invalid!</div>}
         <div>
           <label htmlFor="password">Password: </label>
           <input type="password" name="password" id="password" />
         </div>
-        <input type="submit" name="" value="Login" />
+        <label htmlFor="rememberMe">Remember me </label>
+        <input type="checkbox" name="rememberMe" id="rememberMe" defaultChecked />
+        <input type="submit" value="Login" />
       </form>
     </div>
   );
