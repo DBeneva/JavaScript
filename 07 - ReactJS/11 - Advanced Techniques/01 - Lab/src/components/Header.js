292705ff3f75e5fc511ce92e96eefc4e7688321b
Darina Beneva <beneva.darina@gmail.com>
2021-12-08 22:33:15 +0200

    React Bootstrap

---
+++ 07 - ReactJS/11 - Advanced Techniques/01 - Lab/src/components/Header.js
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import { Nav, Navbar, NavDropdown, Container, Form, FormControl, Button } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Link to="/" as={Navbar.Brand}>Home</Link>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Link to="/login" as={Nav.Link}>Login</Link>
+                        <Nav.Link href="#action2">Link</Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                Something else here
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" disabled>
+                            Link
+                        </Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ 07 - ReactJS/11 - Advanced Techniques/01 - Lab/src/contexts/AuthContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+const initialState = { _id: '', email: '', accessToken: '' };
+
+export const AuthProvider = ({
+    children
+}) => {
+    const [user, setUser] = useState(initialState);
+
+    return (
+        <AuthContext.Provider values={user}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
